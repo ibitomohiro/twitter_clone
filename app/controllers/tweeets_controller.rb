9c54e5c4d8321526d4e79da1c227f5bc6bde3bb4
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-08-26 08:44:43 +0900

    ストロングパラメーターの導入

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -4,4 +4,12 @@ class TweeetsController < ApplicationController
   def new
     @tweeet = Tweeet.new
   end
+  def create 
+    Tweeet.create(tweeet_params)
+    redirect_to new_tweeet_path
+  end
+  private
+  def tweeet_params
+    params.require(:tweeet).permit(:content)
+  end
 end
