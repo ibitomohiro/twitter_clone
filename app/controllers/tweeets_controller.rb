7ae51b8c29971b2744ab3fec9ee20a24b3a36621
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-08-26 22:29:56 +0900

    edit　updateの実装

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -16,6 +16,17 @@ class TweeetsController < ApplicationController
   def show
     @tweeet = Tweeet.find(params[:id])
   end
+  def edit
+    @tweeet = Tweeet.find(params[:id])
+  end
+  def update
+    @tweeet = Tweeet.find(params[:id])
+    if @tweeet.update(tweeet_params)
+      redirect_to tweeets_path, notice: "Tweeetを編集しました！"
+    else  
+      render :edit
+    end
+  end
   private
   def tweeet_params
     params.require(:tweeet).permit(:content)
